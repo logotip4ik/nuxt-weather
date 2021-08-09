9f3ff3bf57fa5524a2435d4a2bc318980fac7876
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-08-09 18:26:48 +0300

    update: some basic setup of city page

---
+++ pages/city/_name.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{ name }}
+    <pre>
+      {{ JSON.stringify(currentWeather, null, 2) }}
+    </pre>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, $axios, $config }) {
+    const { name } = params
+
+    const currentWeatherUrl = $config.currentWeather(
+      params.name,
+      $config.apiKey
+    )
+    const currentWeather = await $axios.$get(currentWeatherUrl)
+
+    return { name, currentWeather }
+  },
+}
+</script>
+
+<style></style>
