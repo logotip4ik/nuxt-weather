ad24d332c1f31060fe649f0f328271f203e6c9b8
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-08-13 15:36:07 +0300

    update: played a bit with backdrop filter

--- components/CurrentWeather.vue
+++ components/CurrentWeather.vue
@@ -88,15 +88,26 @@ export default {
     }
   }
   &__current-time {
-    position: absolute;
-    top: 10%;
-    right: 5vw;
-    transform: transalte(-50%, -50%);
-    background-color: hsla(0, 0%, 50%, 50%);
-    color: var(--font-light);
-    padding: 0.25rem 0.75rem;
-    border-radius: 0.25rem;
+    position: fixed;
+    top: 8%;
+    right: 6vw;
+
     font-size: 1.1rem;
+    color: var(--font-light);
+    border-radius: 0.25rem;
+    padding: 0.25rem 0.75rem;
+    background-color: hsla(0, 0%, 50%, 35%);
+    backdrop-filter: blur(0.2rem);
+
+    // prettier-ignore
+    box-shadow:
+      0 2.8px 2.2px rgba(0, 0, 0, 0.02),
+      0 6.7px 5.3px rgba(0, 0, 0, 0.028),
+      0 12.5px 10px rgba(0, 0, 0, 0.035),
+      0 22.3px 17.9px rgba(0, 0, 0, 0.042),
+      0 41.8px 33.4px rgba(0, 0, 0, 0.05),
+      0 100px 80px rgba(0, 0, 0, 0.07)
+    ;
   }
 }
 </style>
