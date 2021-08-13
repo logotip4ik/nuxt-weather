000d8b2725fcb4c44c01eeb9f5a9b35675f8be1b
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-08-13 15:15:52 +0300

    update: basic layout of hourly forecast

--- components/HourlyForecast.vue
+++ components/HourlyForecast.vue
@@ -1,21 +1,43 @@
 <template>
-  <div class="slider">
-    <div v-for="(hourData, idx) in data" :key="idx" class="slider__item">
-      <span>
-        {{ hourData.temp | normalizeTemp }} /
-        {{ hourData.pop | normalizePercentage }}
-      </span>
-      <span
-        >{{ hourData.dt | normalizeToTime }}
-        {{ hourData.dt | normalizeToDate }}
-      </span>
+  <section class="section">
+    <h3 class="section__heading">Hourly Forecast:</h3>
+    <div class="section__slider">
+      <div
+        v-for="(hourData, idx) in data"
+        :key="idx"
+        class="section__slider__item"
+      >
+        <img
+          :src="`http://openweathermap.org/img/wn/${hourData.weather[0].icon}@2x.png`"
+          :alt="`${hourData.weather[0].main} icon`"
+          loading="lazy"
+          class="section__slider__item__image"
+        />
+        <p class="section__slider__item__heading">
+          {{ hourData.weather[0].main | toTitleCase }}
+        </p>
+        <span>
+          {{ hourData.temp | normalizeTemp }} /
+          {{ hourData.pop | normalizePercentage }}
+        </span>
+        <span
+          >{{ hourData.dt | normalizeToTime }}
+          {{ hourData.dt | normalizeToDate }}
+        </span>
+      </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
 export default {
   filters: {
+    toTitleCase(string) {
+      return string
+        .split('')
+        .map((val, i) => (i === 0 ? val.toUpperCase() : val))
+        .join('')
+    },
     normalizePercentage(percentage) {
       return `${percentage % 1 === 0 ? percentage : percentage.toFixed(1)}%`
     },
@@ -44,74 +66,92 @@ export default {
 <style lang="scss" scoped>
 @use "sass:math";
 
-.slider {
-  $size: 12ch;
-  $gap: 2rem;
+.section {
+  padding-block: 10vh 0;
 
-  display: grid;
-  grid-auto-flow: column;
-  grid-auto-columns: $size;
-  gap: math.div($gap, 2);
-  padding-inline: $gap;
-  padding-block-start: $gap * 2;
-  padding-block-end: $gap;
+  &__heading {
+    font-weight: 400;
+    padding-inline: 5vw;
+    padding-block: 0 5vh;
+  }
 
-  overflow-x: auto;
-  overscroll-behavior-x: contain;
-  scroll-snap-type: x proximity;
-  scroll-padding-inline-start: $gap;
-  width: 100%;
-  position: relative;
-  scrollbar-width: none;
+  &__slider {
+    $size: 12ch;
+    $gap: 2rem;
 
-  &__item {
-    --surface-color: hsla(0, 0%, 0%, 5%);
+    display: grid;
+    grid-auto-flow: column;
+    grid-auto-columns: $size;
+    gap: math.div($gap, 2);
+    padding-inline: $gap;
 
-    scroll-snap-align: start;
-
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    flex-direction: column;
-    margin: 0;
-    // outline: 1px solid red;
-
-    text-align: center;
-    border-radius: 0.25rem;
+    overflow-x: auto;
+    overscroll-behavior-x: contain;
+    scroll-snap-type: x proximity;
+    scroll-padding-inline-start: $gap;
+    width: 100%;
     position: relative;
-    user-select: none;
-    padding: 0.5rem;
-    cursor: pointer;
-    background-color: var(--surface-color);
-    // prettier-ignore
-    box-shadow:
+    scrollbar-width: none;
+
+    &__item {
+      --surface-color: hsla(0, 0%, 0%, 7%);
+
+      scroll-snap-align: start;
+
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      flex-direction: column;
+      margin: 0;
+      // outline: 1px solid red;
+
+      text-align: center;
+      border-radius: 0.25rem;
+      position: relative;
+      user-select: none;
+      padding: 0.5rem;
+      cursor: pointer;
+      background-color: var(--surface-color);
+      // prettier-ignore
+      box-shadow:
       0 0.1px 0.7px rgba(0, 0, 0, 0.004),
       0 0.2px 2.2px rgba(0, 0, 0, 0.006),
       0 1px 10px rgba(0, 0, 0, 0.01)
     ;
 
-    transition: background-color 0.3s ease;
+      transition: background-color 0.3s ease;
 
-    // &:hover,
-    // &:focus {
-    //   background-color: hsl(0, 0%, 50%, 0.2);
-    //   transition: none;
-    // }
+      // &:hover,
+      // &:focus {
+      //   background-color: hsl(0, 0%, 50%, 0.2);
+      //   transition: none;
+      // }
 
-    &:last-child::after {
-      content: '';
-      position: absolute;
-      width: $gap;
-      height: 100%;
-      right: $gap * 2;
-      inline-size: $gap;
-      block-size: 100%;
-      inset-block-start: 0;
-      inset-inline-end: $gap * 2;
-    }
+      &:last-child::after {
+        content: '';
+        position: absolute;
+        width: $gap;
+        height: 100%;
+        left: $size;
+        inline-size: $gap;
+        block-size: 100%;
+        inset-block-start: 0;
+        inset-inline-end: $gap * 2;
+      }
 
-    @media (prefers-color-scheme: dark) {
-      --surface-color: hsla(0, 0%, 0%, 15%);
+      &__image {
+        width: 6ch;
+        height: auto;
+        display: block;
+      }
+      &__heading {
+        padding-block-end: 0.5rem;
+        font-size: 1.1rem;
+      }
+
+      @media (prefers-color-scheme: dark) {
+        --surface-color: hsla(0, 0%, 0%, 11%);
+      }
     }
   }
 }
